6e458dbde5cfdc1de37e4675ad3143628d2d4a86
oluwafemi medalem <oluwafemi.medale@andela.com>
2018-03-09 10:33:42 +0100

    fix todo user

--- app/graphql/resolvers/create_todo.rb
+++ app/graphql/resolvers/create_todo.rb
@@ -1,14 +1,16 @@
 class Resolvers::CreateTodo < GraphQL::Function
   argument :desc, !types.String
   argument :name, !types.String
+  argument :user_id, !types.ID
 
   type Types::TodoType
 
   def call(obj, args, ctx)
+    user =  User.find(args[:user_id])
     Todo.create!(
       desc: args[:desc],
       name: args[:name],
-      user: ctx[:current_user]
+      user: user
     )
   end
 end
